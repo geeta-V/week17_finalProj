52aa54329b466d5283fe3cd840f8e42c3667d34f
geeta-V <133255958+geeta-V@users.noreply.github.com>
2025-02-23 18:47:18 -0600

    Update register.py

--- data-science/src/register.py
+++ data-science/src/register.py
@@ -6,41 +6,62 @@ import mlflow
 import pandas as pd
 from pathlib import Path
 
-mlflow.start_run()  # Starting the MLflow experiment run
+def parse_args():
+    '''Parse input arguments'''
 
-def main():
-    # Argument parser setup for command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", type=str, help="Name of the model to register")  # Model name for registration
-    parser.add_argument("--model_path", type=str, help="Path to the trained model")  # Path to the trained model artifact
-    parser.add_argument("--model_info_output_path", type=str, help="Path to save model info")  # Path to save model info
-    args = parser.parse_args()
+    parser.add_argument('--model_name', type=str, help='Name under which model will be registered')
+    parser.add_argument('--model_path', type=str, help='Model directory')
+    parser.add_argument("--model_info_output_path", type=str, help="Path to write model info JSON")
+    args, _ = parser.parse_known_args()
+    print(f'Arguments: {args}')
 
-    # Load the trained model from the provided path
-    model = mlflow.sklearn.load_model(args.model_path)  # _ (Fill the code to load model from args.model)
+    return args
 
-    print("Registering the best trained used cars price prediction model")
-    print(f"Registering model: {args.model_name}")
-    
-    # Register the model in the MLflow Model Registry under the name "price_prediction_model"
+def main(args):
+    '''Loads the best-trained model from the sweep job and registers it'''
+
+    print("Registering ", args.model_name)
+
+    #  Load the trained model from the provided path
+    model = mlflow.sklearn.load_model(args.model_path)
+
+    # Log model using mlflow
     mlflow.sklearn.log_model(
         sk_model=model,
         registered_model_name=args.model_name,  # Specify the name under which the model will be registered
         artifact_path="random_forest_price_regressor"  # Specify the path where the model artifacts will be stored
     )
 
-     # Optionally, you could log some model information to a file
-    model_info = {
-        "model_name": args.model_name,
-        "model_path": args.model_path,
-        "status": "success"
-    }
-    # Save model info to the provided output path
-    model_info_df = pd.DataFrame([model_info])
-    model_info_df.to_csv(args.model_info_output_path, index=False)
+    # Register logged model using mlflow
+    run_id = mlflow.active_run().info.run_id
+    model_uri = f'runs:/{run_id}/{args.model_name}'
+    mlflow_model = mlflow.register_model(model_uri, args.model_name)
+    model_version = mlflow_model.version
 
-    # End the MLflow run
-    mlflow.end_run()  # __ (Fill in the code to end the MLflow run)
+    # Write model info
+    print("Writing JSON")
+    model_info = {"id": f"{args.model_name}:{model_version}"}
+    output_path = os.path.join(args.model_info_output_path, "model_info.json")
+    with open(output_path, "w") as of:
+        json.dump(model_info, of)
 
 if __name__ == "__main__":
-    main()
+    
+    mlflow.start_run()
+    
+    # Parse Arguments
+    args = parse_args()
+    
+    lines = [
+        f"Model name: {args.model_name}",
+        f"Model path: {args.model_path}",
+        f"Model info output path: {args.model_info_output_path}"
+    ]
+
+    for line in lines:
+        print(line)
+
+    main(args)
+
+    mlflow.end_run()
